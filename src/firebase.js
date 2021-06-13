35fffdfff3da0ae95091908943e2e6da553c291f
meddyassin <meddyassin@hotmail.com>
2021-06-13 02:56:31 +0100

    l

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPg7ngKrvsvzXCbbg9vLd0-QVBoi0pouY",
-  authDomain: "ratethefuck.firebaseapp.com",
-  projectId: "ratethefuck",
-  storageBucket: "ratethefuck.appspot.com",
-  messagingSenderId: "1002335066207",
-  appId: "1:1002335066207:web:75bf95f0a806b43f37a3ea",
-  measurementId: "G-1RSPWTP933"
+  apiKey: "AIzaSyAReochBY_zafy_Qp_edWb8h4JAvDFkXt0",
+  authDomain: "ratethefuck-v2.firebaseapp.com",
+  projectId: "ratethefuck-v2",
+  storageBucket: "ratethefuck-v2.appspot.com",
+  messagingSenderId: "1038267520340",
+  appId: "1:1038267520340:web:3790d8b632a302d98a64c1",
+  measurementId: "G-T9VKX7XYFX"
   };
 
 firebase.initializeApp(firebaseConfig);
