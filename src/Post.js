6bea49b9bf987a3853bef61a3707de0343a35293
Mohamed Yassin <71594166+meddyassin@users.noreply.github.com>
2021-07-12 01:10:29 +0200

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -1,26 +1,26 @@
 import React from "react";
 import VoteButtons from "./vote-buttons";
-const Post = ({ fuck }) => {
+const Post = ({ fuck, q }) => {
   return (
-    <div>
+    <div className="the_fucking_post">
     <div className="post">
       {fuck.saythefuck === "" || !fuck.saythefuck ? "" : <div>
       <p className="post_text">{fuck.saythefuck}</p>
-      <VoteButtons fuck={fuck} />
+      <VoteButtons fuck={fuck} q={q} />
       </div>}
      </div>
      <div className="post">
       {fuck.name === "" || fuck.roastthefuck === "" || !fuck.name || !fuck.roastthefuck ? "" :  <div className="post">
       <p className="the_name_post">{fuck.name}</p>
       <p>{fuck.roastthefuck}</p>
-      <VoteButtons fuck={fuck} />
+      <VoteButtons fuck={fuck} q={q} />
       </div>}
      </div>
      <div className="post">
       {fuck.answer === "" || !fuck.answer ? "" :  <div className="post">
       <p className="the_question_post">{fuck.question}</p>
       <p>{fuck.answer}</p>
-      <VoteButtons fuck={fuck} className="the_bottom_thing" />
+      <VoteButtons fuck={fuck} q={q} className="the_bottom_thing" />
       </div>}
      </div>
     </div>
